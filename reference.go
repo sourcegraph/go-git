48b9f27a99097ad70927e39c9fbd012b0c0337f7
Unknown <joe2010xtmf@163.com>
2014-04-12 20:36:10 -0400

    Add unpack refs

--- reference.go
+++ reference.go
@@ -212,3 +212,26 @@ func (r *Reference) AllCommits() (*list.List, error) {
 	err := r.repository.commitsBefore(lock, l, nil, r.Oid, 0)
 	return l, err
 }
+
+// UnpackRefs unpacks 'packed-refs' to git repository.
+func UnpackRefs(repoPath string) error {
+	refs, err := ioutil.ReadFile(filepath.Join(repoPath, "packed-refs"))
+	if err != nil {
+		return err
+	}
+
+	for _, ref := range strings.Split(string(refs), "\n") {
+		if len(ref) == 0 || ref[0] == '#' {
+			continue
+		} else if !strings.Contains(ref, "heads") && !strings.Contains(ref, "tags") {
+			continue
+		}
+
+		infos := strings.Split(ref, " ")
+		os.MkdirAll(path.Dir(filepath.Join(repoPath, infos[1])), os.ModePerm)
+		if err = ioutil.WriteFile(filepath.Join(repoPath, infos[1]), []byte(infos[0]), os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
