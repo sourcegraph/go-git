bd7c5c9a2aca48b346d79800d06f8f2fae027fb7
Lunny Xiao <xiaolunwen@gmail.com>
2014-05-05 09:37:34 +0800

    add write blob

--- blob.go
+++ blob.go
@@ -1,30 +1,18 @@
 package git
 
-import ()
+import (
+	"bytes"
+	"compress/zlib"
+	sha "crypto/sha1"
+	"fmt"
+	"io"
+)
 
 type Blob struct {
 	*TreeEntry
 
 	data   []byte
 	dataed bool
-
-	size  int64
-	sized bool
-}
-
-func (b *Blob) Size() int64 {
-	if b.sized {
-		return b.size
-	}
-
-	size, err := b.ptree.repo.objectSize(b.Id)
-	if err != nil {
-		return 0
-	}
-
-	b.sized = true
-	b.size = size
-	return b.size
 }
 
 func (b *Blob) Data() ([]byte, error) {
@@ -38,4 +26,34 @@ func (b *Blob) Data() ([]byte, error) {
 	b.data = data
 	b.dataed = true
 	return b.data, nil
+}
+
+func (b *Blob) Save(w io.Writer, compress bool) (string, error) {
+	var data []byte
+	buf := bytes.NewBuffer(data)
+	header := fmt.Sprintf("blob %d\\0", len(b.data))
+	_, err := buf.Write([]byte(header))
+	if err != nil {
+		return "", err
+	}
+	_, err = buf.Write(b.data)
+	if err != nil {
+		return "", err
+	}
+	if compress {
+		var b bytes.Buffer
+		c := zlib.NewWriter(&b)
+		_, err = io.Copy(c, buf)
+		if err != nil {
+			return "", err
+		}
+		_, err = io.Copy(w, &b)
+	} else {
+		_, err = io.Copy(w, buf)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", sha.Sum(data)), nil
 }
